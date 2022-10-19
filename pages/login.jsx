86939ac35f6e05d0d00ce90e67ea4a19c453134a
MarcosPassos-git <marcosbrunopassos@gmail.com>
2022-10-18 23:19:08 -0300

    Criado componentes e tema

--- pages/login.jsx
+++ pages/login.jsx
@@ -5,6 +5,8 @@ import ImageWithSpace from '../src/components/layout/ImageWithSpace'
 import H1 from '../src/components/typography/H1'
 import H2 from '../src/components/typography/H2'
 import H4 from '../src/components/typography/H4'
+import Button from '../src/components/inputs/Button'
+import Input from '../src/components/inputs/Input'
 
 const FormContainer = styled.div`
     margin-top: 60px;
@@ -13,7 +15,7 @@ const FormContainer = styled.div`
 const Form = styled.form`
     display: flex;
     flex-direction: column;
-    margin: 20px, 0;
+    margin: 20px 0;
     gap: 20px;
 `
 
@@ -29,9 +31,9 @@ function LoginPage () {
             <FormContainer>
                 <H2>Entre em sua conta</H2>
                 <Form>
-                    <input placeholder="Email ou usuário" type="email" />
-                    <input placeholder="Senha" type="password" />
-                    <button>Entrar</button>
+                    <Input label="Email ou usuário" type="email" />
+                    <Input label="Senha" type="password" />
+                    <Button>Entrar</Button>
                 </Form>
                 <Text>Não possui uma conta? <a href='#'>Faça seu cadastro</a></Text>
             </FormContainer>
---
+++ src/components/inputs/Input.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const inputContainer = styled.div`
+    width: 100%;
+`
+
+const StyledLabel = styled.p`
+    font-weight: bold;
+    font-size: 14px;
+    margim-bottom: 5px;
+`
+
+const StyledInput = styled.input`
+    width: 100%;
+    border: 1px solid ${props => props.theme.inputBorder};
+    background-color: ${props => props.theme.inputBackground};
+    padding: 15px 20px;
+    box-sizing: border-box;
+    border-radius: 10px
+`
+
+function Input ({label, ...props}) {
+    return (
+        <inputContainer>
+            <StyledLabel>{label}</StyledLabel>
+            <StyledInput placeholder={label} {...props} />
+        </inputContainer>
+    )
+}
+
+export default Input
